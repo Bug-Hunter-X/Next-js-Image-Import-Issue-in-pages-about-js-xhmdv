faf22f1df2cf7b62999d9c4d2882c3ba4104e7cd
BugHunter-X <askzenai@gmail.com>
2024-12-10 17:32:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,29 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* This is the problematic part */}
+      <img src="/image.png" alt="About Page Image" />
+    </div>
+  );
+}
+
+export default About;
+```
